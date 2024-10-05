24325624a660438fccbfa0ae3f5f9dfd878ea9fc
Bekmurod <bekmurod20051117@gmail.com>
2024-10-05 14:46:32 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+// alert("Salomxush kelibsiz!");
+// const place=confirm("salomlar");
+// console.log(place);
+// const currentPlace=prompt("where are you from")
+// console.log(currentPlace);
+// const age=+prompt("how old are you?")
+// console.log(age+78);
+
+
